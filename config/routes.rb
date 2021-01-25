0d495ef939ce76e0b69b89c807badfe6c66d9f85
Kyle Zimmerman <kzdesignsios@gmail.com>
2021-01-24 16:36:21 -0800

    created custom action for updating blog status from draft to published

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'portfolio/edit/:id', to: 'portfolios#edit', as: 'portfolio_edit'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status 
+    end
+  end
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
